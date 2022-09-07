43fed011e4e0e7a25ae319a697df2a72a4aeb03c
AHMED NABIB <nabibpallab@gmail.com>
2022-09-07 23:45:23 +0600

    update quantity is fimished

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,33 +1,52 @@
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import Product from "../Product/product";
-import Cart from '../Cart/Cart';
+
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 
 import "./Shop.css";
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart]  = useState([])
+	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState([]);
 
-    const handleAddToCart = (product) => {
-        console.log(product);
-        const newCart = [...cart, product];
-        setCart(newCart);
-    }
+	const handleAddToCart = (product) => {
+		// console.log(product);
+		const newCart = [...cart, product];
+		setCart(newCart);
+		addToDb(product.id);
+	};
 	useEffect(() => {
 		fetch("products.json")
 			.then((res) => res.json())
 			.then((data) => setProducts(data));
 	}, []);
+
+	useEffect(() => {
+		const storedCart = getStoredCart();
+		const savedCart = [];
+		for (const id in storedCart) {
+			const addedProduct = products.find((product) => product.id === id);
+			if (addedProduct) {
+				const quantity = storedCart[id];
+				addedProduct.quantity = quantity;
+				savedCart.push(addedProduct);
+			}
+		}
+		setCart(savedCart);
+	}, [products]);
+
 	return (
 		<div className="shop-container">
 			<div className="products-container">
 				{products.map((product) => (
-                    <Product key={product.id} product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>
+					<Product
+						key={product.id}
+						product={product}
+						handleAddToCart={handleAddToCart}></Product>
 				))}
 			</div>
 			<div className="cart-container">
-                <Cart cart={cart}></Cart>
+				<Cart cart={cart}></Cart>
 			</div>
 		</div>
 	);
